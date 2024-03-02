12f46be508e9ba98066172779dfa0cef6bc0167d
Bread <77288737+riley0122@users.noreply.github.com>
2024-03-02 15:34:33 +0100

    Added tailwind and electron

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadURL(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8080"
+      : `file://${__dirname}/index.html`
+  );
+};
+
+app.on("ready", createWindow);
