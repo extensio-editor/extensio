5652256e648d0c39ac931448270b76b3b84dcbc4
Bread <77288737+riley0122@users.noreply.github.com>
2024-03-02 17:05:45 +0100

    removed tailwind and did some stuff

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <img alt="extensio logo" src="./assets/logo.png" class="logo" />
+  <router-view class="view left-sidebar" name="LeftSidebar"></router-view>
+  <router-view class="view main-content"></router-view>
+  <router-view class="view right-sidebar" name="RightSidebar"></router-view>
+  <router-view class="view bottom-bar" name="BottomBar"></router-view>
+  <router-view class="view top-bar" name="TopBar"></router-view>
 </template>
 
 <style lang="less">
+@background-col: #302f3dff;
+@foreground-col: #c1c0c9ff;
+.logo {
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  left: 10px;
+  top: 10px;
+  border-radius: 2px;
+}
+
+body {
+  background-color: @background-col;
+  color: @foreground-col;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: @foreground-col;
+  background-color: @background-col;
 }
 </style>
